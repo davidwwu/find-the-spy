e69c07ba486b7b0156cb25c6e0dbb29fd426c030
Glitch (hello-express) <none>
2019-08-25 02:29:51 +0000

    🎄🍴 Checkpoint ./package.json:3794427/1081 ./user.js:3794427/537 ./server.js:3794427/2931 ./public/app.js:3794427/3495 ./public/style.css:3794427/1732 ./public/index.html:3794427/1390 ./public/chat.html:3794427/2104 ./public/materialize/mat.js:3794427/543315 ./public/materialize/mat.css:3794427/141829

--- server.js
+++ server.js
@@ -1,22 +1,81 @@
-// server.js
-// where your node app starts
+const express = require("express");
+const socket = require("socket.io");
+const path = require("path");
+const http = require("http");
+const users = require("./user")();
 
-// init project
-const express = require('express');
+const publicPath = path.join(__dirname, "./public");
+const port = process.env.PORT || 3000;
+
 const app = express();
+const server = http.createServer(app);
+const io = socket(server);
 
-// we've started you off with Express, 
-// but feel free to use whatever libs or frameworks you'd like through `package.json`.
+const message = (name, text, id) => {
+    return {
+        name,
+        text,
+        id
+    };
+}
 
-// http://expressjs.com/en/starter/static-files.html
-app.use(express.static('public'));
+app.use(express.static(publicPath));
 
-// http://expressjs.com/en/starter/basic-routing.html
-app.get('/', function(request, response) {
-  response.sendFile(__dirname + '/views/index.html');
-});
+io.on(
+    "connection",
+    (sock) => {
+        console.dir("IO connection <-");
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, function() {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+        sock.on("join", (user, cb) => {
+            if (!user.name || !user.room) {
+                return cb("Enter valid user data");
+            } else {
+                cb({userId: sock.id});
+                sock.join(user.room);
+
+                users.remove(sock.id);
+                users.add(sock.id, user.name, user.room);
+
+                io.to(user.room).emit("users:update", users.getUsersByRoom(user.room));
+                sock.emit("message:new", message("admin", `welcome, ${user.name}`));
+                sock.broadcast.to(user.room).emit("message:new", message("Admin", `${user.name} joined`));
+                
+            }
+        });
+
+        sock.on(
+            "message:create",
+            (data, cb) => {
+                if (!data) {
+                    cb("message cant be empty");
+                } else {
+                    const user = users.get(sock.id);
+                    if (user) {
+                        io.to(user.room).emit("message:new", message(data.name, data.text, data.id))
+                    }
+                    cb();
+                }
+                console.log(data.text);
+            }
+        );
+
+        sock.on("disconnect", ()=>{
+            const user = users.remove(sock.id);
+            if (user) {
+                io.to(user.room).emit(
+                    "message:new", 
+                    message("admin", `${user.name} left the chat`)
+                );
+                io.to(user.room).emit(
+                    "users:update", 
+                    users.getUsersByRoom(user.room)
+                );
+            }
+        });
+    }
+);
+
+server.listen(
+    port,
+    () => console.log(`Server has been started on ${port} port`)
+);
