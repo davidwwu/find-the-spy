b7ad118ad4cc83eba4aa495991490e5bd591c905
Glitch (hello-express) <none>
2020-09-11 22:51:53 +0000

    👱🐾 Checkpoint ./game.js:1932921/1245

---
+++ game.js
@@ -0,0 +1,49 @@
+class Game {
+  constructor() {
+    this.gameInProgress = false;
+    this.players = [];
+    this.gameSetup = {
+      '平民': 0,
+      '臥底': 0,
+      '白板': 0
+    }
+  }
+  
+  start()
+
+  add(id, name, room, status, role) {
+    this.users.push({ id, name, room, status, role });
+  }
+
+  get(id) {
+    return this.users.find(u => u.id === id);
+  }
+  
+  update(newUserInfo) {
+    this.get(newUserInfo.id).name = newUserInfo.name;
+    this.get(newUserInfo.id).room = newUserInfo.room;
+    this.get(newUserInfo.id).status = newUserInfo.status;
+    this.get(newUserInfo.id).role = newUserInfo.role;
+    console.log(`newUser: ${this.get(newUserInfo.id)}`);
+  }
+
+  remove(id) {
+    const user = this.get(id);
+    if (user) {
+      this.users = this.users.filter(u => u.id !== user.id);
+    }
+    return user;
+  }
+
+  getUsersByRoom(room) {
+    return this.users.filter(u => u.room === room);
+  }
+  
+  getReadiedPlayersByRoom(room) {
+    return this.users.filter(u => u.room === room && u.status === '已坐下');
+  }
+}
+
+module.exports = function() {
+  return new Game();
+};
